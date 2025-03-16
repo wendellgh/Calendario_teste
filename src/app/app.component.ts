d811c934930b6b96483d3964768538735dd1152e
Wendell Mendes <wendellgh@gmail.com>
2025-03-16 16:06:34 -0300

    Exibindo o calendario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import { CalendarOptions } from '@fullcalendar/core/index.js';
+import interactionPlugin from '@fullcalendar/interaction';
+import dayGridPlugin from '@fullcalendar/daygrid';
+// import timeGridPlugin from '@fullcalendar/timegrid';
+// import listPlugin from '@fullcalendar/list';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FullCalendarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Calendario_teste';
+  calendarVisible = true;
+  calendarOptions = signal<CalendarOptions> ({
+  plugins: [
+    dayGridPlugin,
+    interactionPlugin],
+  
+  })
+
+handleDateClick(){
+  alert('date click! ')
+}
+
 }
